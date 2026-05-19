46715e7b14b01638189577f05166b28ce3734af4
Joe.Zhuo <joezhuo2018@gmail.com>
2026-05-19 16:22:13 -0400

    added status effect manager

---
+++ Assets/scripts/StatusEffect/StatusEffectManager.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel.Design;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusEffectManager : MonoBehaviour
+{
+    private readonly List<StatusEffect> activeEffects = new();
+
+    public void AddEffect(StatusEffect se, GameObject source)
+    {
+        StatusEffect existing = activeEffects.Find(e => e.GetType() == se.GetType());
+
+        if (existing != null)
+        {
+            if (existing.currentStacks < existing.maxStacks)
+            {
+                existing.currentStacks++;
+                existing.OnStack();
+            }
+            else existing.currentTime = 0;
+        }
+        else
+        {
+            StatusEffect runtimeEffect = Instantiate(se);
+            runtimeEffect.target = gameObject;
+            runtimeEffect.currentStacks = 1;
+            runtimeEffect.source = source;
+            runtimeEffect.currentTime = 0;
+
+            activeEffects.Add(runtimeEffect);
+            runtimeEffect.OnApply();
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = activeEffects.Count - 1; i >= 0; i--)
+        {
+            StatusEffect e = activeEffects[i];
+
+            if (e.tickInterval > 0)
+            {
+                int oldTicks = Mathf.FloorToInt(e.currentTime / e.tickInterval);
+                int newTicks = Mathf.FloorToInt((e.currentTime + Time.deltaTime) / e.tickInterval);
+                if (newTicks > oldTicks) e.OnTick();
+            }
+
+            e.currentTime += Time.deltaTime;
+
+            if (e.currentTime > e.duration)
+            {
+                e.OnExpire();
+                if (activeEffects.Contains(e)) activeEffects.Remove(e);
+            }
+        }
+    }
+}
--- Assets/scripts/StatusEffect/statusEffect.cs
+++ Assets/scripts/StatusEffect/statusEffect.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public abstract class StatusEffect : ScriptableObject
 {
     [Header("Basic")]
+    public float currentTime;
     public float duration; // in seconds
     public float tickInterval; // how often effect triggers
-    protected GameObject target;
-    
+    public GameObject target;
+    public GameObject source;
     [Header("Stacking")]
     public int maxStacks = 1;
     public int currentStacks;
