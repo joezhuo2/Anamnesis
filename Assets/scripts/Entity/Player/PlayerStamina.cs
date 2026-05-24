926c558ffd550793f6a25018f4baf2c0fcb821aa
Joe.Zhuo <joezhuo2018@gmail.com>
2026-05-24 16:39:48 -0400

    added player stamina

---
+++ Assets/scripts/Entity/Player/PlayerStamina.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using System;
+
+public class PlayerStamina : MonoBehaviour
+{
+    private float regenTimer = 0f;
+    private readonly float regenInterval = 0.2f;
+    private readonly float fullRegenFrequency = 5f;
+    private float accumaltedRegen = 0f;
+    public PlayerStats p;
+    public void Update()
+    {
+        if (p.isAlive)
+            RegenStamina();
+    }
+    public void ChangeStamina(int amount)
+    {
+        if (amount > 0 && !p.canGainStamina) return;
+        p.currentStamina = Math.Min(p.currentStamina + amount, p.maxStamina);
+    }
+    public void RegenStamina()
+    {
+        if (p == null || p.currentStamina > p.maxStamina || p.staminaRegen == 0) return;
+
+        regenTimer += Time.deltaTime;
+
+        float regenPerTick = p.staminaRegen / fullRegenFrequency * regenInterval;
+
+        accumaltedRegen += regenPerTick;
+
+        if (accumaltedRegen >= 1f)
+        {
+            int intRegen = Mathf.FloorToInt(accumaltedRegen);
+            accumaltedRegen -= intRegen;
+            ChangeStamina(intRegen);
+        }
+    }
+}
