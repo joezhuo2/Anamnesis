208b243aacc59f67e4ed3f20e9db498e6ada0e85
Joe.Zhuo <joezhuo2018@gmail.com>
2026-06-20 20:05:04 -0400

    for enemies, now scales stats based off level

--- Assets/scripts/Entity/EntityStatManager.cs
+++ Assets/scripts/Entity/EntityStatManager.cs
@@ -8,15 +8,53 @@ public class EntityStatManager : MonoBehaviour
 {
     public EntityStats s;
     public List<StatBuff> currentBuffs = new();
-    private void Awake()
+    private void Start()
     {
         if (s != null) s = Instantiate(s);
+
+        if (gameObject.CompareTag("Enemy"))
+        {
+            if (s.level > 1)
+            {
+                ScaleBaseStats(s.level);
+            }
+            s.currentHp = s.maxHp;
+        }
 
         s.canAttack = true;
         s.isAttacking = false;
         s.canGainHp = true;
         s.isAlive = true;
         s.isImmune = false;
+    }
+    private void ScaleBaseStats(int currentLevel)
+    {
+        int levelOffset = currentLevel - 1;
+
+        s.attack = Mathf.RoundToInt(s.attack * (1f + (0.10f * levelOffset)));
+
+        s.damagePct += 0.01f * levelOffset;
+        s.physicalDmgPct *= (1f + (0.03f * levelOffset));
+        s.spellDmgPct *= (1f + (0.03f * levelOffset));
+
+        s.critChance = Mathf.Clamp(s.critChance * (1f + (0.01f * levelOffset)), 0f, 100f);
+        s.critDamage *= (1f + (0.03f * levelOffset));
+
+        s.aoePct += 0.015f * levelOffset;
+
+        s.maxHp = Mathf.RoundToInt(s.maxHp * (1f + (0.15f * levelOffset)));
+        s.hpRegen = Mathf.RoundToInt(s.hpRegen * (1f + (0.05f * levelOffset)));
+        s.armor = Mathf.RoundToInt(s.armor * (1f + (0.05f * levelOffset)));
+
+        s.damageRes = Mathf.Clamp(s.damageRes + (0.01f * levelOffset), 0f, 0.35f);
+        s.physicalRes = Mathf.Clamp(s.physicalRes + (0.01f * levelOffset), 0f, 0.70f);
+        s.spellRes = Mathf.Clamp(s.spellRes + (0.01f * levelOffset), 0f, 0.70f);
+
+        s.dodgeChance = Mathf.Clamp(s.dodgeChance + (0.5f * levelOffset), 0f, 60f);
+        s.dodgeResPct = Mathf.Clamp(s.dodgeResPct + (0.01f * levelOffset), 0f, 0.80f);
+
+        s.moveSpeed = Mathf.Clamp(s.moveSpeed * (1f + (0.01f * levelOffset)), 0f, 3.0f);
+        s.moveSpeedPct = Mathf.Clamp(s.moveSpeedPct * (1f + (0.01f * levelOffset)), 0f, 2.0f);
     }
 
     private void OnDestroy()
