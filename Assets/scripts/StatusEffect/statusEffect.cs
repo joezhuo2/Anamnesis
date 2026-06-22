bf88ccfca1922e19ef3d686bf80f6495aca37169
Joe.Zhuo <joezhuo2018@gmail.com>
2026-06-22 11:56:31 -0400

    fields that should not be adjustable are now hidden

--- Assets/scripts/StatusEffect/statusEffect.cs
+++ Assets/scripts/StatusEffect/statusEffect.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public abstract class StatusEffect : ScriptableObject
 {
     [Header("Basic")]
-    public float currentTime;
-    public float duration; // in seconds
-    public float tickInterval; // how often effect triggers
-    public GameObject target;
-    public GameObject source;
+    [HideInInspector] public float currentTime;
+    public float duration;
+    [Tooltip("How often effect triggers")]public float tickInterval;
+    [HideInInspector] public GameObject target;
+    [HideInInspector] public GameObject source;
     [Header("Stacking")]
     public int maxStacks = 1;
-    public int currentStacks;
+    [HideInInspector] public int currentStacks = 0;
 
     public virtual void OnTick() {}
     public virtual void OnApply() {}
