efc2c067c8e2a54003e76ba78b88b7d4b1c20737
Joe.Zhuo <joezhuo2018@gmail.com>
2026-06-25 22:22:37 -0400

    improved tooltip spacing

--- Assets/scripts/Entity/Player/TooltipTrigger.cs
+++ Assets/scripts/Entity/Player/TooltipTrigger.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -36,13 +37,13 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         float staminaPerSecond = cps.EffStReg / 5f;
         float healthPerSecond = cps.EffHpReg / 5f;
 
-        string details = "";
-        if (staminaPerSecond != 0) details += $"Stamina: {staminaPerSecond:F1}/s";
-        if (healthPerSecond != 0) details += $"\nHealth: {healthPerSecond:F1}/s";
-        if (cps.EffArmor != 0) details += $"\nArmor: {cps.EffArmor} (-{100f-(cps.ArmorRes*100f):F1}%)";
-        if (cps.damageRes != 0) details += $"\nRes: {cps.damageRes:F1}%";
+        List<string> lines = new();
+        if (staminaPerSecond != 0) lines.Add($"Stamina: {staminaPerSecond:F1}/s");
+        if (healthPerSecond != 0) lines.Add($"Health: {healthPerSecond:F1}/s");
+        if (cps.EffArmor != 0) lines.Add($"Armor: {cps.EffArmor} (-{(cps.ArmorRes*100f):F1}%)");
+        if (cps.damageRes != 0) lines.Add($"Res: {cps.damageRes:F1}%");
 
-        TooltipUI.Instance.ShowTooltip("Resources", details);
+        TooltipUI.Instance.ShowTooltip("Resources", string.Join("\n", lines));
     }
     public void ShowAttackTooltip()
     {
@@ -64,21 +65,24 @@ public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitH
             trueDmg = pd.trueMult * cesm.GetStat(pd.scalingStat) * (1f + (cps.damagePct * 0.01f));
         }
 
-        string details = "";
-        if (cooldown != 0f) details += $"Cooldown: {cooldown:F1}s";
-        if (hpCost != 0f) details += $"\nHealth Cost: {hpCost:F0}";
-        if (hpGain != 0f) details += $"\nHealth Gain: {hpGain:F0}";
-        if (staminaCost != 0f) details += $"\nStamina Cost: {staminaCost:F0}";
-        if (staminaGain != 0f) details += $"\nStamina Gain: {staminaGain:F0}";
-        if (manaCost != 0f) details += $"\nMana Cost: {manaCost:F0}";
-        if (manaGain != 0f) details += $"\nMana Gain: {manaGain:F0}";
-        if (basePhysDmg != 0f) details += $"\nBase: {basePhysDmg:F0}P";
-        if (baseSplDmg != 0f) details += $" {baseSplDmg:F0}S";
-        if (trueDmg != 0f) details += $" {trueDmg:F0}T";
+        List<string> lines = new();
+        if (cooldown != 0f) lines.Add($"Cooldown: {cooldown:F1}s");
+        if (hpCost != 0f) lines.Add($"Health Cost: {hpCost:F0}");
+        if (hpGain != 0f) lines.Add($"Health Gain: {hpGain:F0}");
+        if (staminaCost != 0f) lines.Add($"Stamina Cost: {staminaCost:F0}");
+        if (staminaGain != 0f) lines.Add($"Stamina Gain: {staminaGain:F0}");
+        if (manaCost != 0f) lines.Add($"Mana Cost: {manaCost:F0}");
+        if (manaGain != 0f) lines.Add($"Mana Gain: {manaGain:F0}");
 
-        string title = cad.displayName;
+        List<string> dmgTypes = new();
+        if (basePhysDmg != 0f) dmgTypes.Add($"{basePhysDmg:F0}P");
+        if (baseSplDmg != 0f) dmgTypes.Add($"{baseSplDmg:F0}S");
+        if (trueDmg != 0f) dmgTypes.Add($"{trueDmg:F0}T");
 
-        TooltipUI.Instance.ShowTooltip(title, details);
+        if (dmgTypes.Count > 0)
+            lines.Add($"Base: {string.Join(" ", dmgTypes)}");
+
+        TooltipUI.Instance.ShowTooltip(cad.displayName, string.Join("\n", lines));
     }
     public void OnPointerExit(PointerEventData eventData)
     {
