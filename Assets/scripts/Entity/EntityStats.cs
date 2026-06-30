226b9061dc9354ef8f1b024d7f0b74d429b85a1e
Joe.Zhuo <joezhuo2018@gmail.com>
2026-06-29 20:19:06 -0400

    added paradox (global dot crit)

--- Assets/scripts/Entity/EntityStats.cs
+++ Assets/scripts/Entity/EntityStats.cs
@@ -38,7 +38,8 @@ public enum StatType
     EffStReg,
     EffSpd,
     EffArmor,
-    MaxMana
+    maxMana,
+    globalDoTCanCrit
 }
 
 [CreateAssetMenu(fileName = "stats", menuName = "Scriptable Objects/stats/entity")]
---
+++ Assets/scripts/Entity/Player/PlayerUpgrades/Paradox.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerUpgrade", menuName = "PlayerUpgrade/Paradox")]
+public class Paradox : PlayerUpgrade
+{
+    public override void TriggerUpgradeEffect(GameObject player) {}
+}
