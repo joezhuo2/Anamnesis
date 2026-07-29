20e2e274295d69e00d084fa4db8e5be7b67d1519
Joe.Zhuo <joezhuo2018@gmail.com>
2026-07-29 00:47:05 -0400

    added attaack replacement, exodus, and statbuffs statuys effect

---
+++ Assets/scripts/StatusEffect/StatusEffects/AttackReplacement/AttackReplacement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "se_ar", menuName = "Status Effects/Debuff/Attack Replacement")]
+
+public class AttackReplacement : StatusEffect
+{
+    public AttackData replacement;
+    private AttackData originalAttack = null;
+    private bool setAttack = false;
+
+    public override void OnApply()
+    {
+        if (target == null || replacement == null) return;
+        if (!target.TryGetComponent<PlayerAttackHandler>(out var pah)) return;
+
+        setAttack = true;
+
+        AttackData original = pah.FindAttackOfType(replacement.type);
+        if (original != null) originalAttack = Instantiate(original);
+
+        pah.UpdateAttack(replacement.type, replacement);
+    }
+
+    public override void OnExpire()
+    {
+        if (!setAttack || replacement == null || target == null) return;
+        if (!target.TryGetComponent<PlayerAttackHandler>(out var pah)) return;
+
+        pah.UpdateAttack(replacement.type, originalAttack);
+    }
+
+    private void OnDestroy()
+    {
+        if (originalAttack != null)
+            Destroy(originalAttack);
+    }
+}
---
+++ Assets/scripts/StatusEffect/StatusEffects/StatBuff/StatBuffs.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "se_statbuff", menuName = "Status Effects/Buff/Stat Buffs")]
+public class StatBuffs : StatusEffect
+{
+    public List<StatBuff> buffs = new();
+    public Dictionary<StatBuff, StatBuff> curActiveBuff = new();
+
+    public override void OnApply() => ApplyBuffs();
+    public override void OnStack()
+    {
+        UndoCurrentBuffs();
+        ApplyBuffs();
+    }
+    public override void OnExpire() => UndoCurrentBuffs();
+    private void ApplyBuffs()
+    {
+        if (target == null || !target.TryGetComponent<EntityStatManager>(out var esm)) return;
+
+        foreach (var buff in buffs)
+        {
+            var b = new StatBuff(buff.type, buff.value * currentStacks);
+            esm.AddStat(b, true);
+            curActiveBuff[buff] = b;
+        }
+    }
+    private void UndoCurrentBuffs()
+    {
+        if (target != null && target.TryGetComponent<EntityStatManager>(out var esm))
+        {
+            foreach (var kvp in curActiveBuff)
+                esm.AddStat(kvp.Value, false);
+            curActiveBuff.Clear();
+        }
+    }
+}
